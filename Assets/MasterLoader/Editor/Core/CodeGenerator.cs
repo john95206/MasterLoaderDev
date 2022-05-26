b49fed5eddebdc47037fa242dea3fd0c2daebf47
Yuu <yutakahashi1025@gmail.com>
2022-05-26 13:05:22 +0900

    コンパイルエラーを修正。

--- Assets/MasterLoader/Editor/Core/CodeGenerator.cs
+++ Assets/MasterLoader/Editor/Core/CodeGenerator.cs
@@ -385,13 +385,13 @@ namespace MasterLoader.Core
                 }
 
                 masterListCode +=
-                $"{GetBaseIndent(2)}public {masterName}{MasterLoader.MASTER} {masterName} {{ get {{ return _{masterName} }} }}" +
+                $"{GetBaseIndent(2)}public {masterName}{MasterLoader.MASTER} {publicMasterName} {{ get {{ return _{masterName}; }} }}" +
                 $"{GetBaseIndent(2)}[SerializeField]" +
                 $"{GetBaseIndent(2)}private {masterName}{MasterLoader.MASTER} _{masterName};";
 
                 var path = $"\"{AssetDatabase.GetAssetPath(config.MasterPathFolder)}/{masterName}.asset\"";
                 installCode +=
-                $"{GetBaseIndent(2)}{_TAB}{masterName} = AssetDatabase.LoadMainAssetAtPath({path}) as {masterName}{MasterLoader.MASTER};";
+                $"{GetBaseIndent(2)}{_TAB}_{masterName} = AssetDatabase.LoadMainAssetAtPath({path}) as {masterName}{MasterLoader.MASTER};";
             }
 
             var editorCode =
