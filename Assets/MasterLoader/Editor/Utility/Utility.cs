4a10d6a5ded48c489642381c8d510df106edc69a
Yuu <yutakahashi1025@gmail.com>
2022-05-26 16:59:29 +0900

    文字列のバリデーションを追加

--- Assets/MasterLoader/Editor/Utility/Utility.cs
+++ Assets/MasterLoader/Editor/Utility/Utility.cs
@@ -10,5 +10,44 @@ namespace MasterLoader.Utility
             var assetPath = $"{path}/{name}.asset";
             return AssetDatabase.LoadMainAssetAtPath(assetPath);
         }
+
+        public static bool OnValidateInputedValue(string text)
+        {
+            for(var i = 0; i < text.Length; i++)
+            {
+                var t = text[i];
+                // 先頭文字が数字や記号だったら弾く
+                if(i == 0)
+                {
+                    if (char.IsNumber(t))
+                    {
+                        return false;
+                    }
+                    if (char.IsSymbol(t))
+                    {
+                        for(var v = 0; i < validSymbols.Length; i++)
+                        {
+                            if (t == validSymbols[v])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                // 二文字目以降が数字やアルファベットでも _ でもなかったら弾く
+                if (!char.IsLetterOrDigit(t) && !t.IsValidSymbol())
+                {
+                    return false;
+                }
+            }
+            text.Replace(" ", string.Empty);
+            text.Replace("　", string.Empty);
+            return true;
+        }
+
+        public static bool IsValidSymbol(this char c)
+        {
+            return c != '_';
+        }
     }
 }
