aeeda0c8fb30475aebb18e887dadaf892394d579
Yuu <yutakahashi1025@gmail.com>
2022-05-29 00:32:04 +0900

    MasterInstallerを初期化

--- Assets/MasterLoader/Scripts/Utility/MasterInstaller.cs
+++ Assets/MasterLoader/Scripts/Utility/MasterInstaller.cs
@@ -1,21 +1,25 @@
-﻿using UnityEngine;
+﻿/*
+* ---------------------------------------------
+*this Code is Auto Generated.
+* All Changes will be nothing when regenerated.
+* ---------------------------------------------
+* これは自動生成コードです。
+* このコードに行った変更は自動生成時に破棄されます。
+* ---------------------------------------------
+*/
+
+using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
-using MasterLoader;
-using Dev;
 
 namespace MasterLoader
 {
 	public class MasterInstaller : MonoBehaviour
 	{
-		public testMasterMaster testMaster;
-		public secondMaster second;
 #if UNITY_EDITOR
 		public void SetMaster()
 		{
-			testMaster = AssetDatabase.LoadMainAssetAtPath("Assets/MasterLoader/Master/testMaster.asset") as testMasterMaster;
-			second = AssetDatabase.LoadMainAssetAtPath("Assets/MasterLoader/Master/second.asset") as secondMaster;
 		}
 #endif
 	}
